988fb5f8a14e7f4df8da28dc948e7026098dcbfe
shihab <echoshihab@gmail.com>
2020-11-14 20:12:58 -0500

    limit ship range+Q to quit

--- alien_invasion/ship.py
+++ alien_invasion/ship.py
@@ -26,10 +26,11 @@ class Ship:
 
     def update(self):
         """Update the ship's position based on the movement flag."""
-        if self.moving_right:
-            self.rect.x += self.settings.ship_speed
-        if self.moving_left:
-            self.rect.x -= self.settings.ship_speed
+        # Update the ship's x value, not the rect
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x += self.settings.ship_speed
+        if self.moving_left and self.rect.left > 0:
+            self.x -= self.settings.ship_speed
 
         # Update rect object form self.x
         self.rect.x = self.x
