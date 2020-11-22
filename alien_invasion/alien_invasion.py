b7b7391343c50173de1deb0f3d67714ba0fb8cc9
shihab <echoshihab@gmail.com>
2020-11-22 13:19:04 -0500

    increase difficulty post fleet destruction

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -62,8 +62,10 @@ class AlienInvasion:
 
     def _check_play_button(self, mouse_pos):
         """Start a new game when the player clicks play."""
-        button_clicked= self.play_button.rect.collidepoint(mouse_pos)
+        button_clicked = self.play_button.rect.collidepoint(mouse_pos)
         if button_clicked and not self.stats.game_active:
+            # Reset the game settings.
+            self.settings.initialize_dynamic_settings()
             # Reset the game statistics
             self.stats.reset_stats()
             self.stats.game_active = True
@@ -75,6 +77,9 @@ class AlienInvasion:
             # Create a new fleet and center the ship
             self._create_fleet()
             self.ship.center_ship()
+
+            # Hide the mouse cursor
+            pygame.mouse.set_visible(False)
 
     def _check_keydown_events(self, event):
         """Respond to keypresses."""
@@ -134,6 +139,7 @@ class AlienInvasion:
             # Destroy existing bullets and creat a new fleet
             self.bullets.empty()
             self._create_fleet()
+            self.settings.increase_speed()
 
     def _create_alien(self, alien_number, row_number):
         """Create an alien and place it in the row"""
@@ -217,6 +223,7 @@ class AlienInvasion:
             sleep(0.5)
         else:
             self.stats.game_active = False
+            pygame.mouse.set_visible(True)
 
 
 if __name__ == '__main__':
