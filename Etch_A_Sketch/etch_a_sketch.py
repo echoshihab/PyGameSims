29ec82ee739dd1338a6f0af05eadfe62ba382a09
echoshihab <echoshihab@gmail.com>
2021-02-21 09:51:49 -0500

    Add etch a sketch implementation with turtle

---
+++ Etch_A_Sketch/etch_a_sketch.py
@@ -0,0 +1,28 @@
+from turtle import Turtle, Screen
+
+tim = Turtle()
+screen = Screen()
+
+
+def move_forwards():
+    tim.forward(10)
+
+
+def move_backwards():
+    tim.backward(10)
+
+
+def turn_clockwise():
+    tim.right(15)
+
+
+def turn_counter_clockwise():
+    tim.left(15)
+
+
+screen.listen()
+screen.onkey(fun=move_forwards, key="w")
+screen.onkey(fun=move_backwards, key="s")
+screen.onkey(fun=turn_clockwise, key="d")
+screen.onkey(fun=turn_counter_clockwise, key="a")
+screen.exitonclick()
