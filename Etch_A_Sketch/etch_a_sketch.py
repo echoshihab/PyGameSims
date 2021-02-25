bbcb22288c749d494061d5ce711d048d68c6f35c
echoshihab <echoshihab@gmail.com>
2021-02-24 19:59:07 -0500

    Create Snake and move it

--- Etch_A_Sketch/etch_a_sketch.py
+++ Etch_A_Sketch/etch_a_sketch.py
@@ -19,11 +19,13 @@ def turn_clockwise():
 def turn_counter_clockwise():
     tim.left(15)
 
+
 def clear():
     tim.clear()
     tim.penup()
     tim.home()
     tim.pendown()
+
 
 screen.listen()
 screen.onkey(fun=move_forwards, key="w")
