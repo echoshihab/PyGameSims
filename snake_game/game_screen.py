e707a052c62ec6a2a4fad17f56d435ada197e084
echoshihab <echoshihab@gmail.com>
2021-03-03 18:46:17 -0500

    Detect collisions with wall

--- snake_game/game_screen.py
+++ snake_game/game_screen.py
@@ -9,7 +9,6 @@ screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("Snake Game")
 
-
 game_on = True
 screen.tracer(0)
 
@@ -28,11 +27,16 @@ while game_on:
     screen.update()
     time.sleep(0.1)
     snake.move()
+
+    # Detect collision with food
     if snake.head.distance(food) < 15:
         food.refresh_position()
         scoreboard.update_score()
 
-# detect collision with food
-
+    # Detect collision with wall
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 \
+            or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        scoreboard.game_over()
+        game_on = False
 
 screen.exitonclick()
--- snake_game/scoreboard.py
+++ snake_game/scoreboard.py
@@ -1,20 +1,30 @@
 from turtle import Turtle
 
+# constants
+FONT = ("Arial", 8, "bold")
+STARTING_COORDINATE_X = 0
+STARTING_COORDINATE_Y = 270
+ALIGNMENT = "Center"
+
 
 class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.penup()
         self.score = 0
-        self.goto(0, 270)
+        self.goto(STARTING_COORDINATE_X, STARTING_COORDINATE_Y)
         self.hideturtle()
         self.pencolor("white")
         self.update_text()
 
     def update_text(self):
-        self.write(f"Score: {self.score}")
+        self.write(f"Score: {self.score}", font=FONT, align=ALIGNMENT)
 
     def update_score(self):
         self.score += 1
         self.clear()
         self.update_text()
+
+    def game_over(self):
+        self.goto(0, 0)
+        self.write(f"GAME OVER!", font=FONT, align= ALIGNMENT)
