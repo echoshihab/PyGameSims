577cc67c7e20a6efcf7fb44fb9eb51e067ec2b68
echoshihab <echoshihab@gmail.com>
2021-03-29 08:15:47 -0400

    handle keyerror for letters not in alphabet

--- nato_alphabet_project/nato_alphabet.py
+++ nato_alphabet_project/nato_alphabet.py
@@ -5,7 +5,17 @@ import pandas
 read_csv = pandas.read_csv("nato_phonetic_alphabet.csv")
 alphabet_dict = {row.letter: row.code for index, row in read_csv.iterrows()}
 
-word = input("Type your name: ").upper()
-code_words = [alphabet_dict[item] for item in word]
-print(code_words)
+invalid_input = True
+
+while invalid_input:
+    word = input("Type your name: ").upper()
+    try:
+        code_words = [alphabet_dict[item] for item in word]
+        print(code_words)
+        invalid_input = False
+    except KeyError:
+        print("Sorry, only letters in the alphabet please")
+        invalid_input = True
+
+
 
