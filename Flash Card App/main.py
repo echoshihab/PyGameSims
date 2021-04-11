201d0007e085cc8464223451c3c72f24a592adcb
echoshihab <echoshihab@gmail.com>
2021-04-11 15:17:09 -0400

    Complete flash card game

--- Flash Card App/main.py
+++ Flash Card App/main.py
@@ -4,21 +4,30 @@ import random
 import time
 BACKGROUND_COLOR = "#B1DDC6"
 
-csv = pd.read_csv("data/french_words.csv")
-main_list = csv.to_dict(orient='records')
+try:
+    csv = pd.read_csv("data/words_to_learn.csv")
+except FileNotFoundError:
+    csv = pd.read_csv("data/french_words.csv")
+
+main_dict = csv.to_dict(orient='records')
 current_card = {}
 
 
 def get_next_card():
     global current_card, flip_timer
     window.after_cancel(flip_timer)
-    current_card = random.choice(main_list)
+    current_card = random.choice(main_dict)
     random_french = current_card["French"]
     canvas.itemconfig(foreground, image=foreground_img)
     canvas.itemconfig(card_title, text="French", fill="black")
     canvas.itemconfig(card_word, text=random_french, fill="black")
-    flip_timer= window.after(3000, func=flip_card)
+    flip_timer = window.after(3000, func=flip_card)
 
+def card_known():
+    main_dict.remove(current_card)
+    new_data = pd.DataFrame(main_dict)
+    new_data.to_csv("data/words_to_learn.csv", index=False)
+    get_next_card()
 
 
 def flip_card():
@@ -50,7 +59,7 @@ unknown_button = Button(image=cross_image, highlightthickness=0, command=get_nex
 unknown_button.grid(row=1, column=0)
 
 check_image = PhotoImage(file="images/right.png")
-known_button = Button(image=check_image, highlightthickness=0, command=get_next_card)
+known_button = Button(image=check_image, highlightthickness=0, command=card_known)
 known_button.grid(row=1, column=1)
 
 canvas.grid(row=0, column=0)
